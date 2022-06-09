968f52f2c67941661bcd5a426cd1f522a4e788fd
Iago <iagoblopez@gmail.com>
2022-06-09 17:06:00 +0200

    Create class ComicListController.java

---
+++ src/main/java/com/cifprodolfo/comic_store/ComicListController.java
@@ -0,0 +1,16 @@
+package com.cifprodolfo.comic_store;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ComicListController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
