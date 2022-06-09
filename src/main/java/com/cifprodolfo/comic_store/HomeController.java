882198af2408e3357e2552b1a9a7b90200d61c12
Iago <iagoblopez@gmail.com>
2022-06-09 23:23:50 +0200

    Implementing methods to view the selected screen.fxml

--- src/main/java/com/cifprodolfo/comic_store/HomeController.java
+++ src/main/java/com/cifprodolfo/comic_store/HomeController.java
@@ -1,14 +1,32 @@
 package com.cifprodolfo.comic_store;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HomeController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private VBox PanelFrame;
+
+    public void getPanelConfiguration(){
+
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(HomeController.class.getResource("configuration.fxml"));
+            VBox voxConfiguration = fxmlLoader.load();
+
+            PanelFrame.getChildren().add(voxConfiguration);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
